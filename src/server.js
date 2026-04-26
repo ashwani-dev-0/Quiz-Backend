f24e6e3af9d0f8b7fe7bbfa42e31f1d48299f85d
Ashwani <you@example.com>
2026-04-26 11:52:11 +0530

    fixed cors and port

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" });
+
 import express from "express";
 import userRouter from "./routes/users.route.js";
 import quizRouter from "./routes/quiz.route.js";
@@ -14,15 +15,16 @@ const server = express();
 
 server.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://quiz-frontend-six-indol.vercel.app",
   })
 );
+
 server.use(express.json());
 
-// Public routes (no auth required)
+// Public routes
 server.use("/api", authRouter);
 
-// Protected routes (require valid JWT)
+// Protected routes
 server.use(authMiddleware);
 
 server.use("/api", userRouter);
@@ -30,6 +32,8 @@ server.use("/api", quizRouter);
 server.use("/api", quesRouter);
 server.use("/api", feedbackRouter);
 
-server.listen(8181, () => {
-  console.log(`server started on port ${8181}`);
+const PORT = process.env.PORT || 8181;
+
+server.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
 });
